14c99851361cbbff86467ff63505b0ae1811a095
andrelinces <andrelinces@yahoo.com.br>
2024-05-28 19:32:09 -0300

    Starting test mode, no changes :q1

---
+++ Test_Mode/Test_Mode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_Mode
+//
+//  Created by Andre Linces on 5/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
